3a7df1a5870f8ca5f76114589adbdf81cd0f4836
Peter Ajtai <peter.ajtai@thinksolid.com>
2015-05-27 07:35:35 -0700

    finishing up current set of options

---
+++ Vagrantfile
@@ -0,0 +1,50 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+# Vagrant plugins used:
+# vagrant-exec
+# vagrant-vbguest
+
+Vagrant.configure("2") do |config|
+  config.vbguest.auto_update = true
+
+  # do NOT download the iso file from a webserver
+  config.vbguest.no_remote = false
+
+  config.vm.provider "virtualbox" do |v|
+      v.customize ["setextradata", :id, "VBoxInternal2/SharedFoldersEnableSymlinksCreate/vagrant", "1"]
+
+      host = RbConfig::CONFIG['host_os']
+
+        # Give VM 1/4 system memory & access to all cpu cores on the host
+        if host =~ /darwin/
+          cpus = `sysctl -n hw.ncpu`.to_i
+          # sysctl returns Bytes and we need to convert to MB
+          mem = `sysctl -n hw.memsize`.to_i / 1024 / 1024 / 4
+        elsif host =~ /linux/
+          cpus = `nproc`.to_i
+          # meminfo shows KB and we need to convert to MB
+          mem = `grep 'MemTotal' /proc/meminfo | sed -e 's/MemTotal://' -e 's/ kB//'`.to_i / 1024 / 4
+        else # sorry Windows folks, I can't help you
+          cpus = 2
+          mem = 1024
+        end
+
+        v.customize ["modifyvm", :id, "--memory", mem]
+        v.customize ["modifyvm", :id, "--cpus", cpus]
+  end
+
+  config.vm.box = "ubuntu/trusty64"
+
+  config.vm.network :forwarded_port, guest: 80, host: 8080, auto_correct: true
+  config.vm.network :forwarded_port, guest: 3306, host: 8306, auto_correct: true
+
+  config.vm.network "private_network", ip: "192.168.99.99"
+  config.vm.synced_folder "./", "/var/www/inlineplastics.com/source", nfs: true
+
+  config.vm.provision :ansible do |ansible|
+     ansible.playbook = "ansible/site.yml"
+     ansible.inventory_path = "ansible/hosts"
+     ansible.limit = "vagrant"
+     ansible.verbose = "v"
+  end
+end
--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 #!/usr/bin/env node
 
-var inquirer = require("inquirer"),
-    path = require('path');
+var inquirer = require('inquirer'),
+    BB = require('bluebird'),
+    exec = require('child_process').exec,
+    fs = BB.promisifyAll(require('fs')),
+    path = require('path'),
+    cwd = process.cwd(),
+    defaults = [
+        'apt',
+        'essentials',
+        'git',
+        'info',
+        'pip'
+    ],
+    pga = require('commander'),
+    verbose = false;
+
+pga
+    .version(require('./package.json').version)
+    .option('-v, --verbose', 'Verbose output')
+    .parse(process.argv);
+
+verbose = pga.verbose;
+
+verbose && console.log('verbose output on.');
+
+// TODO: put version number check around this
+// Hack to bump EventEmitter.defaultMaxListeners for older versions of node:
+// https://github.com/joyent/node/issues/3215
+var EventEmitter = require('events').EventEmitter,
+    on = EventEmitter.prototype.on;
+EventEmitter.prototype.on = function () {
+    this._maxListeners = Infinity;
+    on.apply(this, arguments);
+};
 
 inquirer
     .prompt([
@@ -11,24 +43,95 @@ inquirer
             message : 'Which roles do you need?',
             choices : [
                 'elastic-search',
-                'essentials',
-                'git',
                 'mongo',
                 'mysql',
                 'nginx',
                 'node',
                 'oh-my-zsh',
-                'php-fpm',
-                'pip'
+                'php-fpm'
             ]
         }
     ], function( answers ) {
-        console.log(answers);
 
-        answers.roles.forEach(function (role) {
-            fs.readFile(path.join('ansible', role), function (err, data) {
-                if (err) throw err;
-                console.log(data);
+        var ansibleDir = path.join(cwd, 'ansible');
+        fs
+            .statAsync(ansibleDir)
+            .catch(function(stat) {
+                if ('ENOENT' === stat.code) {
+                    return fs.mkdirAsync(ansibleDir)
+                }
+            })
+            .then(function() {
+                return createDirectories(answers);
+            })
+            .then(function() {
+                return fs.readFileAsync(path.join(cwd, 'ansible', 'site.yml'))
+            })
+            .then(function(siteFile) {
+                siteFile +=
+                    '\n' +
+                    answers.roles
+                        .map(function(role) {
+                            return '    - ' + role;
+                        })
+                        .join('\n') +
+                    '\n';
+
+                verbose && console.log('site.yml\n', siteFile);
+                return siteFile;
+            })
+            .then(function(siteFile) {
+                return fs.writeFileAsync(path.join(cwd, 'ansible', 'site.yml'), siteFile);
+            })
+            .catch(function(error) {
+                console.log('WHOOPS:', error);
             });
-        });
     });
+
+function createDirectories(answers) {
+    var copyDirs = defaults.concat(answers.roles).map(copyDir),
+        copyFiles = ['hosts', 'site.yml']
+            .map(copyFile)
+            .concat([
+                runCommand('cp ' + path.join(__dirname, 'Vagrantfile') + ' ' + cwd)
+            ]);
+
+        return BB
+            .all(copyDirs.concat(copyFiles))
+            .catch(function(err) {
+                console.log('something went wrong:', err);
+            });
+}
+
+function copyDir(role) {
+    return runCommand('cp -R ' + path.join(__dirname, 'ansible', role) + ' ' + path.join(cwd, 'ansible'));
+}
+
+function copyFile(file) {
+    return runCommand('cp ' + path.join(__dirname, 'ansible', file) + ' ' + path.join(cwd, 'ansible'));
+}
+
+function runCommand(command) {
+    return new BB(function(resolve, reject) {
+        var childProcess;
+
+        verbose && console.log('running:', command);
+        childProcess = exec(command, {
+            cwd: cwd,
+            customFds: [0,1,2]
+        }, function(err) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve();
+            }
+        });
+        captureOutput(childProcess);
+    });
+}
+
+function captureOutput(childProcess) {
+    childProcess.stdout.pipe(process.stdout);
+    childProcess.stderr.pipe(process.stderr);
+    return childProcess;
+}
